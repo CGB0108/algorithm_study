17f20ac3187f67eecc1921650f63f14e275b8cc6
CGB0108 <lcs970108@gmail.com>
2023-05-14 23:24:06 +0900

    2675_문자열 반복

---
+++ baekjoon/src/class1/Pro2675.java
@@ -0,0 +1,35 @@
+package class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Pro2675 {
+
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int count = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < count; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+
+            String str = st.nextToken();
+
+            for(int j = 0; j < str.length(); j++ ){
+                for(int k = 0; k < num; k++){
+                    System.out.print(str.charAt(j)); 
+                }
+            }
+            System.out.println();
+            
+        }
+      
+       
+    }
+}
