1ab0e74f4c8ec6f2e3f11822e95173d6ecbfb37e
CGB0108 <lcs970108@gmail.com>
2023-11-02 23:23:32 +0900

    1181_단어정렬

---
+++ baekjoon/src/class2/Pro1181.java
@@ -0,0 +1,45 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Pro1181 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] array = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = br.readLine();
+
+        }
+        Arrays.sort(array, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+
+        });
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(array[0]).append('\n');
+        for (int i = 1; i < n; i++) {
+            if (!array[i].equals(array[i - 1])) {
+                sb.append(array[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
